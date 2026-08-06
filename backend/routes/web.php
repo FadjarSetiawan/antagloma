24bbe471e9c92277e10b937428f7fd472a3362ab
Fadjar Setiawan <fadjarsetiawan10@gmail.com>
2026-08-06 20:13:31 +0700

    fix: update web.php to serve React SPA index.html for web routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
+    $indexPath = public_path('index.html');
+    if (file_exists($indexPath)) {
+        return response()->file($indexPath);
+    }
     return response()->json(['message' => 'Antagloma Florist API System']);
-});
+})->where('any', '.*');
